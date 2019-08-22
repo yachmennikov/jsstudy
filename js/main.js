2bf41c70e53d7d03bdcab5a1e81be807c4d9c23a
Yachmennikov Sergey <otsmmm2013@gmail.com>
2019-08-22 23:12:17 +0300

    feat: create new files | lesson001

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const money = 10000,
+      income = 5000,
+      addExpenses = '10%',
+      deposit = 20000,
+      mission = 'slave the world',
+      period = 'untill tomorrow';
+
+alert('enslaved world');
+
+console.log(`I got ${deposit} dollars for ${mission}. But it doesn't enough.`);
+
+
