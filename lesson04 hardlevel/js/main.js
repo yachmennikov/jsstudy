43ab9f25d0a38b131080a0c31860c6ac2c706686
Yachmennikov Sergey <otsmmm2013@gmail.com>
2019-08-28 23:34:01 +0300

    lesson4 version 3.4

--- lesson04 hardlevel/js/main.js
+++ lesson04 hardlevel/js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-let anyThing = prompt('Введи что-нибудь');
 
 function getAnything(thing) {
     if (typeof thing !== 'string') {
@@ -9,9 +8,22 @@ function getAnything(thing) {
     thing = thing.trim();
     console.log(thing)
     if(thing.length > 30) {
-        thing = thing.substring(0, 29);
+        parseText(thing, 30)
     }
-    console.log(thing)
+    console.log(parseText(thing, 30));
 }
 
-getAnything(anyThing);
+let parseText = function(text, limit){
+    if (text.length > limit){
+        for (let i = limit; i > 0; i--){
+            if(text.charAt(i) === ' ' && (text.charAt(i-1) != ','||text.charAt(i-1) != '.'||text.charAt(i-1) != ';')) {
+                return text.substring(0, i) + '...';
+            }
+        }
+         return text.substring(0, limit) + '...';
+    }
+    else
+        return text;
+  };
+
+getAnything('Введи значение');
