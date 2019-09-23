5bd36495a8c6ea80a4ee4582286db4c43b70c9ec
Yachmennikov Sergey <otsmmm2013@gmail.com>
2019-09-23 13:02:48 +0300

    lesson14 v1

---
+++ lesson14/js/script.js
@@ -0,0 +1,165 @@
+window.addEventListener('DOMContentLoaded',  () => {
+'use strict';
+    
+    // timer
+    const countTimer = (deadline) => {
+    
+        const timerHours = document.querySelector('#timer-hours'),
+              timerMinutes = document.querySelector('#timer-minutes'),
+              timerSeconds = document.querySelector('#timer-seconds');
+  
+              const getTimeRemaining = () => {
+                  let dateStop = new Date(deadline).getTime(),
+                      dateNow = new Date().getTime(),
+                      timeRemaining = (dateStop - dateNow) / 1000,
+                      seconds = Math.floor(timeRemaining % 60),
+                      minutes = Math.floor( (timeRemaining / 60) % 60),
+                      hours = Math.floor(timeRemaining / 60 / 60);
+  
+                      return {timeRemaining, hours, minutes, seconds}
+              };
+                 
+              const countDown = () => {
+                  let timer = getTimeRemaining();
+
+                  const conditions = (item) => {
+                      return item < 10 ? '0' + item : item;      
+                  };
+
+                  timerHours.textContent = conditions(timer.hours);
+                  timerMinutes.textContent = conditions(timer.minutes);
+                  timerSeconds.textContent = conditions(timer.seconds);
+
+                  if (timer.timeRemaining < 0) {
+                      clearInterval(stopDeadline);
+                      timerHours.textContent = '00';
+                      timerMinutes.textContent = '00';
+                      timerSeconds.textContent = '00';
+                  };
+                    
+              };  
+              const stopDeadline = setInterval(countDown, 1000);  
+      };
+      
+    countTimer('21 september 2019');
+ 
+    const toggleMenu = () => {
+
+            const btnMenu = document.querySelector('.menu'),
+                menu = document.querySelector('menu'),
+                body = document.querySelector('body');
+            
+            
+                btnMenu.addEventListener('click', () => {
+                    menu.classList.add('active-menu');
+                });
+            
+                body.addEventListener(('click'), (event) => {
+                let target = event.target;
+            
+                if(target.closest('menu') && menu.classList.contains('active-menu')) { 
+                    if(target.tagName !== 'MENU') {
+                        if(target.tagName === 'A'){ 
+                            menu.classList.remove('active-menu');
+                        }
+                    }
+                } else if (!target.closest('menu') && !target.closest('.menu')) {
+                    menu.classList.remove('active-menu');
+                }
+            
+            });
+    };
+
+    toggleMenu();
+
+       
+    // popUp
+    const togglePopUp = () => {
+         const popup = document.querySelector('.popup'),
+                popupBtn = document.querySelectorAll('.popup-btn'),
+                popupClose = document.querySelector('.popup-close'),
+                popupContent = document.querySelector('.popup-content');
+                  
+                popupBtn.forEach( item => item.addEventListener('click', () => { 
+                      popup.style.display = 'block';
+                      animate();
+                 }) 
+            );
+                popupClose.addEventListener('click', (event) => { 
+                    event.stopPropagation();   
+                    popup.style.display = 'none' } );
+                popup.addEventListener('click', () => { popup.style.display = 'none' } );
+                popupContent.addEventListener('click', (event) => { 
+                    event.stopPropagation();  
+                    popup.style.display = 'block' });
+
+            // Animation
+            let count = 0,
+                interval;
+            const animate = () => {
+                if (window.innerWidth > 612) {
+                    interval = requestAnimationFrame(animate);
+                    count +=5;
+                    if (count < 175) {
+                        popupContent.style.top = count + 'px';
+                    } else {
+                        cancelAnimationFrame(interval);
+                        count = 0;
+                    }
+                } else {
+                    cancelAnimationFrame(interval);
+                }
+            };
+        };
+    togglePopUp();
+
+    // Tabs
+    const tabs = () => {
+            const tabHeader = document.querySelector('.service-header'),
+                  tab = tabHeader.querySelectorAll('.service-header-tab'),
+                  tabContent = document.querySelectorAll('.service-tab');
+
+            const toggleTabContent = (index) => {
+                for (let i = 0; i < tabContent.length; i++) {
+                    if (index === i) {
+                        tab[i].classList.add('active');
+                        tabContent[i].classList.remove('d-none');
+                    } else {
+                        tab[i].classList.remove('active');
+                        tabContent[i].classList.add('d-none');
+                    }
+                }
+            };
+                  tabHeader.addEventListener('click', (event) => {
+                    let target = event.target;
+                        target = target.closest('.service-header-tab');
+
+                        if (target) {
+                            tab.forEach( (item, i) => {
+                                if (item === target) {
+                                    toggleTabContent(i);
+                                }
+                            });
+                        }
+                  });
+    };
+    tabs();
+
+       
+        
+        
+        
+       
+           
+     
+
+   
+
+
+
+
+       
+   
+   
+
+});
