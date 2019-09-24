227679d3442e58079d910bbafced8e11197250b9
Yachmennikov Sergey <otsmmm2013@gmail.com>
2019-09-24 23:35:38 +0300

    lesson15-2 v1

--- lesson15-2/js/script.js
+++ lesson15-2/js/script.js
@@ -264,16 +264,49 @@ window.addEventListener('DOMContentLoaded',  () => {
     };
     changeImg();
     
-    const calculate = () => {
-        const calcBlock = document.querySelector('.calc-block');
-        calcBlock.addEventListener('input', (event) => {
+    // calculator
+    const calc = (price = 100) => {
+       const calcBlock = document.querySelector('.calc-block'),
+             calcType = document.querySelector('.calc-type'),
+             calcSquare = document.querySelector('.calc-square'),
+             calcDay = document.querySelector('.calc-day'),
+             calcCount = document.querySelector('.calc-count'),
+             totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1,
+                typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+                if (calcCount.value > 1) {
+                    countValue += (calcCount.value - 1) / 10;
+                };
+
+                if (calcDay && calcDay.value < 5) {
+                    dayValue *= 2;
+                } else if (calcDay && calcDay.value < 10) {
+                    dayValue *= 1.5;
+                }
+
+                if (typeValue && squareValue) {
+                    total = price * typeValue * squareValue * countValue * dayValue;
+                }
+
+            totalValue.textContent = total;
+        };
+        
+        calcBlock.addEventListener('change', (event) => {
             let target = event.target;
-            if (target.matches('.calc-item')) {
-                target.value.replace(/\d/, '');
+            if (target === calcType || target === calcSquare ||
+                target === calcDay || target === calcCount) {
+                countSum();
             }
         })
+
     };
-    calculate();
+    calc();
         
         
        
